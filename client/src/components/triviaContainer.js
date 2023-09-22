19a58993ac6f556f5dd48ed74f4be5cf549eb372
KBradshaw64 <130187631+KBradshaw64@users.noreply.github.com>
2023-09-21 19:05:43 -0700

    remade file pathing for pages

--- client/src/components/triviaContainer.js
+++ client/src/components/triviaContainer.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import NavTabs from './navTabs';
-import Home from './pages/home';
-import Quiz from './pages/quizpage';
-import Highscores from './pages/highscores';
-import Signup from './pages/signup';
-import Login from './pages/login'
+import Home from '../pages/homepage';
+import Quiz from '../pages/quizpage';
+import Highscores from '../pages/highscores';
+import Signup from '../pages/signup';
+import Login from '../pages/login'
